9e112d0a9c988ca94b99bea9663b4f707a89d374
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2025-12-31 17:59:57 +0100

    feat(cli): integrate sinks selection and graceful shutdown in root command
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

--- cmd/dorc/cli/root.go
+++ cmd/dorc/cli/root.go
@@ -1,11 +1,22 @@
 package cli
 
 import (
+	"os"
+
 	"github.com/Grolleau-Benjamin/Dynamic_Onion_Routing/internal/client"
+	"github.com/Grolleau-Benjamin/Dynamic_Onion_Routing/internal/client/sinks/stdout"
+	stui "github.com/Grolleau-Benjamin/Dynamic_Onion_Routing/internal/client/sinks/tui"
 	"github.com/spf13/cobra"
 )
 
 var (
+	logLevel  string
+	onionPath string
+	dest      string
+	payload   string
+
+	tui bool
+
 	rootCommand = &cobra.Command{
 		Use:   "dorc",
 		Short: "Dynamic Onion Routing client",
@@ -22,8 +33,62 @@ func Execute() error {
 	return rootCommand.Execute()
 }
 
-func init() {}
+func init() {
+	rootCommand.Flags().StringVar(
+		&logLevel,
+		"log-level",
+		"info",
+		"Set log level [debug, info, warn, error, off]",
+	)
+	rootCommand.Flags().StringVar(&onionPath,
+		"onion-path",
+		"",
+		"Path of relays to use. e.g. [cafe::1]:62503,127.0.0.1:123 | 1.2.3.4:5678",
+	)
+	rootCommand.Flags().StringVar(&dest,
+		"dest",
+		"",
+		"Final IP. e.g. 8.8.8.8:63",
+	)
+	rootCommand.Flags().StringVar(&payload,
+		"payload",
+		"",
+		"Payload to send to dest (hex-encoded)",
+	)
+
+	rootCommand.Flags().BoolVar(&tui,
+		"tui",
+		false,
+		"Enable TUI mode",
+	)
+}
 
 func Run(cmd *cobra.Command, args []string) {
-	client.Run()
+	ic := client.InputConfig{
+		OnionPath: onionPath,
+		Dest:      dest,
+		Payload:   payload,
+	}
+
+	c := client.New()
+
+	type Sinker interface {
+		Start() error
+	}
+	var s Sinker
+
+	if tui {
+		s = stui.New(c, ic)
+	} else {
+		if !ic.IsComplete() {
+			cmd.PrintErrln("Err: some flags in required flags are missing (onion-path, dest, payload).")
+			os.Exit(1)
+		}
+		s = stdout.New(c, ic)
+	}
+
+	if err := s.Start(); err != nil {
+		cmd.PrintErrln("Error running client:", err)
+		os.Exit(1)
+	}
 }
