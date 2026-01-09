2b5d7161ba46d67118beb3077b35a53f5d6f635d
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2026-01-09 15:33:22 +0100

    feat(crypto): add chacha20poly1305 fonctions
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

---
+++ internal/crypto/chacha20poly1305.go
@@ -0,0 +1,31 @@
+package crypto
+
+import "golang.org/x/crypto/chacha20poly1305"
+
+func ChachaEncrypt(
+	key [32]byte,
+	nonce [12]byte,
+	plaintext []byte,
+	aad []byte,
+) ([]byte, error) {
+	aead, err := chacha20poly1305.New(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	return aead.Seal(nil, nonce[:], plaintext, aad), nil
+}
+
+func ChachaDecrypt(
+	key [32]byte,
+	nonce [12]byte,
+	ciphertext []byte,
+	aad []byte,
+) ([]byte, error) {
+	aead, err := chacha20poly1305.New(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	return aead.Open(nil, nonce[:], ciphertext, aad)
+}
