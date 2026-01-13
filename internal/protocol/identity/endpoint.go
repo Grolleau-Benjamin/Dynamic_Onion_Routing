d733c290210b751531ee23ad91cedda9e25ddba8
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2026-01-13 10:18:58 +0100

    feat(protocol) add endpoint parsing
    Signed-off-by: Benjamin Grolleau <benjamin.grolleau@outlook.com>

--- internal/protocol/identity/endpoint.go
+++ internal/protocol/identity/endpoint.go
@@ -40,13 +40,44 @@ func (e Endpoint) Bytes() ([]byte, error) {
 }
 
 func (e Endpoint) BytesLen() int {
-	if e.IP.To4() != nil {
-		return 1 + 2 + 4 // Type + Port + len(ip)
+	if ip4 := e.IP.To4(); ip4 != nil {
+		return 7
 	}
-	if e.IP.To16() != nil {
-		return 1 + 2 + 16
+	if len(e.IP) > 0 {
+		return 19
 	}
 	return 0
+}
+
+func (e *Endpoint) Parse(data []byte) (int, error) {
+	if len(data) < 3 {
+		return 0, fmt.Errorf("data too short")
+	}
+
+	ipType := data[0]
+	e.Port = binary.BigEndian.Uint16(data[1:3])
+
+	var n int
+	switch ipType {
+	case EndpointIPv4:
+		n = 7
+		if len(data) < n {
+			return 0, fmt.Errorf("buffer too short for IPv4")
+		}
+		e.IP = make(net.IP, net.IPv4len)
+		copy(e.IP, data[3:7])
+	case EndpointIPv6:
+		n = 19
+		if len(data) < n {
+			return 0, fmt.Errorf("buffer too short for IPv6")
+		}
+		e.IP = make(net.IP, net.IPv6len)
+		copy(e.IP, data[3:19])
+	default:
+		return 0, fmt.Errorf("unknown IP type: 0x%02x", ipType)
+	}
+
+	return n, nil
 }
 
 func NewEndpoint(ipStr string, port uint16) (Endpoint, error) {
